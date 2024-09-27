6ebe1e04dce804cdd40f659ffdd74630ad46a85e
Jow <john.sama64@gmail.com>
2024-09-27 20:23:09 -0300

    Conversor de Moedas/ Exercício Estático

---
+++ ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace exercicioEstatico
+{
+    internal class ConversorDeMoeda
+    {
+        public static double IOF = 0.06;
+
+        public static double Operacao(double cotacao, double quantidade)
+        {
+            return (quantidade * cotacao) + (IOF * (cotacao * quantidade));
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using exercicioEstatico;
+using System.Globalization;
+
+Console.Write("Qual é a cotação do dólar? ");
+double cotacao = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+Console.Write("Quantos dólares você vai comprar? ");
+double dolares = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+double reais = ConversorDeMoeda.Operacao(cotacao, dolares);
+
+Console.WriteLine("Valor a ser pago em reais = " +reais.ToString("F2", CultureInfo.InvariantCulture));
